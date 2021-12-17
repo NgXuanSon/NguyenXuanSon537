6987cacdbe1943f9d323f5fd6aec4d0a1085bba6
XuanSon <89241809+NgXuanSon@users.noreply.github.com>
2021-12-17 15:07:36 +0700

    First_commit_NXS537

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NguyenXuanSon537.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
